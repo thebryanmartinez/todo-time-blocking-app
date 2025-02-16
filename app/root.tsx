51779b6aca2effe3d944ead274bf7e6c90e1fda8
Bryan Martínez <bryanmartz972@gmail.com>
2025-02-16 10:41:01 -0600

    feat: add supabase connection and functionality

--- app/root.tsx
+++ app/root.tsx
@@ -3,11 +3,14 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
+  ScrollRestoration,
+  useLoaderData
 } from '@remix-run/react'
 import type { LinksFunction } from '@remix-run/node'
 
 import './tailwind.css'
+import { createClient } from '@supabase/supabase-js'
+import { useState } from 'react'
 
 export const links: LinksFunction = () => [
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -21,6 +24,15 @@ export const links: LinksFunction = () => [
     href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap'
   }
 ]
+
+export const loader = () => {
+  const env = {
+    SUPABASE_PROJECT_URL: process.env.SUPABASE_PROJECT_URL,
+    SUPABASE_API_KEY: process.env.SUPABASE_API_KEY
+  }
+
+  return Response.json({ env })
+}
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -44,5 +56,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />
+  const { env } = useLoaderData<typeof loader>()
+  const [supabase] = useState(() =>
+    createClient(env.SUPABASE_PROJECT_URL, env.SUPABASE_API_KEY)
+  )
+  return <Outlet context={{ supabase }} />
 }
