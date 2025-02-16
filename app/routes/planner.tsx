5f1fd86ce7cb281c0a0b1b18db4347a6f8c3e4d3
Bryan Martínez <bryanmartz972@gmail.com>
2025-02-16 15:05:07 -0600

    feat: add initial Planner page for adding tasks

---
+++ app/routes/planner.tsx
@@ -0,0 +1,91 @@
+import { useOutletContext } from '@remix-run/react'
+import React, { useEffect, useState } from 'react'
+import { ACCESS_TOKEN_STORAGE_KEY } from '~/modules/shared/constants'
+import { Tables } from '~/modules/shared/models'
+
+export default function Planner() {
+  const { supabase } = useOutletContext<OutletContextType>()
+  // const userAccessToken = localStorage.getItem(ACCESS_TOKEN_STORAGE_KEY)
+
+  const [addTask, setAddTask] = useState<Record<string, string>>({
+    title: '',
+    description: '',
+    priority: '',
+    status: ''
+  })
+  const [tasks, setTasks] = useState<Tables<'tasks'>[]>([])
+  const [taskAdded, setTaskAdded] = useState<boolean>(false)
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const { data, error } = await supabase.from('tasks').insert({
+      title: addTask.title,
+      description: addTask.description,
+      priority: addTask.priority,
+      status: addTask.status
+    })
+
+    if (error) {
+      console.log(error)
+      return error
+    }
+
+    if (data) {
+      setTaskAdded(!taskAdded)
+    }
+  }
+
+  useEffect(() => {
+    supabase
+      .from('tasks')
+      .select()
+      .then((data) => setTasks(data.data))
+  }, [taskAdded])
+
+  return (
+    <div className='flex h-screen w-screen flex-col items-center justify-center gap-4'>
+      <form
+        className='flex flex-col gap-4'
+        onSubmit={handleSubmit}
+      >
+        <input
+          type='text'
+          className='input'
+          placeholder='Enter your title'
+          onChange={(e) => setAddTask({ ...addTask, title: e.target.value })}
+        />
+        <input
+          type='text'
+          className='input'
+          placeholder='Enter your description'
+          onChange={(e) =>
+            setAddTask({ ...addTask, description: e.target.value })
+          }
+        />
+        <select
+          className='select'
+          onChange={(e) => setAddTask({ ...addTask, priority: e.target.value })}
+        >
+          <option value='low'>Low</option>
+          <option value='medium'>Medium</option>
+          <option value='high'>High</option>
+        </select>
+        <select
+          className='select'
+          onChange={(e) => setAddTask({ ...addTask, status: e.target.value })}
+        >
+          <option value='pending'>Pending</option>
+          <option value='in_progress'>In Progress</option>
+          <option value='completed'>Completed</option>
+        </select>
+        <button
+          className='btn btn-primary'
+          type='submit'
+        >
+          Add Task
+        </button>
+      </form>
+      <div>{JSON.stringify(tasks)}</div>
+    </div>
+  )
+}
